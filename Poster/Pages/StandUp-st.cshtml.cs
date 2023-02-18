eeb21ef48c031155dd1ba7405d0a6793726435cf
zxcpaladin <pso_03@mail.ru>
2023-02-18 12:20:09 +0500

    Все страницы поиска

---
+++ Poster/Pages/StandUp-st.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Poster.Pages
+{
+    public class StandUp_stModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
