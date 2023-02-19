62fbc60df1b9f85534590a21cc9a2ce6c0d7746a
zxcpaladin <pso_03@mail.ru>
2023-02-19 21:52:29 +0500

    переход на статью

---
+++ Poster/Pages/Information/Sobolev.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Poster.Pages.Information
+{
+    public class SobolevModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Poster/Pages/StandUpSt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Poster.Pages
+{
+    public class StandUpStModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
